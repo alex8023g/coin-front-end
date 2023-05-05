3fa044e623addf4ff1ea0ad9612f079bcc3996b4
alex8023 <alex8023@yandex.ru>
2023-05-05 16:35:25 +0300

    AccountPage almost done

--- coin/src/AccountPage/AccountPage.tsx
+++ coin/src/AccountPage/AccountPage.tsx
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { Button, Paper, TextField } from '@mui/material';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { IAccount } from '../HomePage';
+import { IAccount, ITransaction } from '../HomePage';
 import {
   Bar,
   BarChart,
@@ -15,6 +15,12 @@ import {
   YAxis,
 } from 'recharts';
 
+interface IBalance {
+  amount: number;
+  monthStr: string;
+  monthNum: number;
+}
+
 export function AccountPage() {
   const [accData, setAccData] = useState<IAccount>({
     account: '',
@@ -22,25 +28,44 @@ export function AccountPage() {
     main: true,
     transactions: [{ amount: 0, date: '', from: '', to: '' }],
   });
+  const [balanceArr, setBalanceArr] = useState<IBalance[]>([]);
+  const [lastTrans, setLastTrans] = useState<ITransaction[]>([]);
   const { account } = useParams();
   // console.log(window.location.href.split('/').at(-1));
 
   const token = sessionStorage.getItem('auth');
-  const monthName = {
-    '01': 'янв',
-    '02': 'фев',
-    '03': 'мар',
-    '04': 'апр',
-    '05': 'май',
-    '06': 'июн',
-    '07': 'июл',
-    '08': 'авг',
-    '09': 'сен',
-    '10': 'окт',
-    '11': 'ноя',
-    '12': 'дек',
-  };
-  console.log(monthName);
+  // const monthName: { [N: string]: string } = {
+  //   '01': 'янв',
+  //   '02': 'фев',
+  //   '03': 'мар',
+  //   '04': 'апр',
+  //   '05': 'май',
+  //   '06': 'июн',
+  //   '07': 'июл',
+  //   '08': 'авг',
+  //   '09': 'сен',
+  //   '10': 'окт',
+  //   '11': 'ноя',
+  //   '12': 'дек',
+  // };
+
+  const monthNameArr = [
+    '',
+    'янв',
+    'фев',
+    'мар',
+    'апр',
+    'май',
+    'июн',
+    'июл',
+    'авг',
+    'сен',
+    'окт',
+    'ноя',
+    'дек',
+  ];
+
+  // console.log(monthNameArr);
 
   useEffect(() => {
     fetch(process.env.REACT_APP_API_SERVER + '/account/' + account, {
@@ -51,50 +76,65 @@ export function AccountPage() {
       },
     })
       .then((res) => res.json())
-      .then((res) => {
-        console.log(res.payload);
-        setAccData(res.payload);
+      .then(({ payload }: { payload: IAccount }) => {
+        console.log(payload);
+        setAccData(payload);
+        setLastTrans(payload.transactions.slice(0, 5));
+        let indexTr = payload.transactions.length - 1;
+        let indexBal = 1;
+
+        const balance: IBalance[] = [];
+        if (payload.transactions[indexTr].date) {
+          balance.push({
+            amount: payload.transactions[indexTr].amount,
+            monthStr:
+              monthNameArr[
+                Number(payload.transactions[indexTr].date.split('-')[1])
+              ],
+            monthNum: Number(payload.transactions[indexTr].date.split('-')[1]),
+          });
+        } else return;
+
+        do {
+          const monthNum = Number(
+            payload.transactions[indexTr].date.split('-')[1]
+          );
+          // console.log(balance, { indexBal }, balance[indexBal]);
+          if (balance[indexBal - 1].monthStr === monthNameArr[monthNum]) {
+            // console.log('if');
+            balance[indexBal - 1].amount +=
+              payload.transactions[indexTr].amount;
+            indexTr--;
+          } else if (
+            [1, -11].includes(balance[indexBal - 1].monthNum - monthNum)
+          ) {
+            console.log('else if');
+            balance.push({
+              amount: payload.transactions[indexTr].amount,
+              monthStr: monthNameArr[monthNum],
+              monthNum,
+            });
+            indexBal++;
+            indexTr--;
+          } else {
+            console.log('else', { indexBal }, balance);
+            const monthNum2 =
+              balance[indexBal - 1].monthNum > 1
+                ? balance[indexBal - 1].monthNum - 1
+                : 12;
+            balance.push({
+              amount: 0,
+              monthStr: monthNameArr[monthNum2],
+              monthNum: monthNum2,
+            });
+            indexBal++;
+            // indexTr--;
+          }
+        } while (balance.length < 6);
+        console.log(balance);
+        setBalanceArr(balance.reverse());
       });
   }, [token]);
-
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-  ];
 
   return (
     <>
@@ -166,7 +206,8 @@ export function AccountPage() {
             <BarChart
               // width={600}
               // height={165}
-              data={data}
+              // data={data}
+              data={balanceArr}
             >
               <CartesianGrid
                 // vertical={false}
@@ -176,9 +217,9 @@ export function AccountPage() {
                 stroke="#000"
               />
               {/* <CartesianAxis /> */}
-              <XAxis dataKey="name" tickLine={false} />
+              <XAxis dataKey="monthStr" tickLine={false} />
               <YAxis orientation="right" tickCount={2} tickLine={false} />
-              <Bar dataKey="uv" fill="#116ACC" />
+              <Bar dataKey="amount" fill="#116ACC" />
             </BarChart>
           </ResponsiveContainer>
         </Paper>
@@ -195,7 +236,46 @@ export function AccountPage() {
         }}
       >
         <h2>История переводов</h2>
-        <div>{accData.transactions[0].date.split('-')[1]}</div>
+        {/* <div>{accData.transactions[0].date.split('-')[1]}</div> */}
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th className={styles.th + ' ' + styles.brLeft}>
+                Счет отправителя
+              </th>
+              <th className={styles.th}>Счет получателя</th>
+              <th className={styles.th}>Сумма</th>
+              <th className={styles.th + ' ' + styles.brRight}>Дата</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lastTrans.map((trans) => {
+              if (trans.from === accData.account) {
+                return (
+                  <tr className={styles.tr}>
+                    <td>{trans.from}</td>
+                    <td>{trans.to}</td>
+                    <td className={styles.colorRed}>- {trans.amount} ₽</td>
+                    <td>
+                      {trans.date.slice(0, 10).split('-').reverse().join('.')}
+                    </td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr className={styles.tr}>
+                    <td>{trans.from}</td>
+                    <td>{trans.to}</td>
+                    <td className={styles.colorGreen}>+ {trans.amount} ₽</td>
+                    <td>
+                      {trans.date.slice(0, 10).split('-').reverse().join('.')}
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
       </Paper>
     </>
   );

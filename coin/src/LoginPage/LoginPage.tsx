2ab13ba732c22852686b9a31d4a08e72df7b1a80
alex8023 <alex8023@yandex.ru>
2023-05-11 13:44:59 +0300

    updt LoginPage

--- coin/src/LoginPage/LoginPage.tsx
+++ coin/src/LoginPage/LoginPage.tsx
@@ -4,11 +4,18 @@ import { Navigate } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export function LoginPage() {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
-  // const [tokenSt, setTokenSt] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const token = sessionStorage.getItem('auth');
 
   function handleLogin(e: ChangeEvent<HTMLInputElement>) {
@@ -17,6 +24,11 @@ export function LoginPage() {
 
   function handlePassword(e: ChangeEvent<HTMLInputElement>) {
     setPassword(e.target.value);
+  }
+
+  function handleSubmit2(e: React.FormEvent) {
+    e.preventDefault();
+    console.log('submit', process.env.REACT_APP_API_SERVER);
   }
 
   function handleSubmit(e: React.FormEvent) {
@@ -53,7 +65,7 @@ export function LoginPage() {
   return (
     <>
       {token && <Navigate to="/" />}
-      <form className={styles.loginForm} onSubmit={handleSubmit}>
+      {/* <form className={styles.loginForm} onSubmit={handleSubmit}>
         <h2 className={styles.h2}>Вход в аккаунт</h2>
         <div>
           Логин{' '}
@@ -64,40 +76,92 @@ export function LoginPage() {
           <input id="password" type="password" onChange={handlePassword} />
         </div>
         <button type="submit">Войти</button>
-      </form>
-      <Paper
-        elevation={7}
-        sx={{
-          padding: '25px 50px',
-          // flexBasis: 500,
-          borderRadius: 9,
-          backgroundColor: '#F3F4F6',
-        }}
+      </form> */}
+      <Box
+        component="form"
+        // position="relative"
+        // top="20%"
+        height="65vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        // onSubmit={(e) => {
+        //   e.preventDefault();
+        //   console.log('submit');
+        // }}
+        onSubmit={handleSubmit}
       >
-        <h2>Новый перевод</h2>
-        <div className={styles.newRemittanceCont}>
-          <div className={styles.textFieldCont}>
+        <Paper
+          elevation={7}
+          sx={{
+            padding: '50px 85px 50px 40px',
+            // width: 500,
+            height: '280px',
+            // flexBasis: 500,
+            borderRadius: 9,
+            backgroundColor: '#F3F4F6',
+          }}
+        >
+          <h2 className={styles.h2}>Вход в аккаунт</h2>
+
+          <div className={styles.loginCont}>
+            <span className={styles.spanLogPass}>Логин</span>
             <TextField
               required
               id="outlined-basic2"
               label="Логин"
               variant="outlined"
-              sx={{ marginBottom: '25px', width: 300 }}
+              onChange={handleLogin}
+              sx={{ width: 300 }}
             />
-            <TextField
+          </div>
+          <div className={styles.passCont}>
+            <span className={styles.spanLogPass}>Пароль</span>
+            {/* <TextField
               required
               id="outlined-basic2"
               label="Пароль"
               type="password"
               variant="outlined"
+              onChange={handlePassword}
               sx={{ width: 300 }}
-            />
+            /> */}
+
+            <FormControl sx={{ width: 300 }} variant="outlined" required>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Пароль
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                onChange={handlePassword}
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => {
+                        setShowPassword(!showPassword);
+                      }}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Пароль"
+              />
+            </FormControl>
           </div>
-          <Button variant="contained" sx={{ padding: '14px 40px' }}>
+
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ marginLeft: '77px', padding: '14px 40px' }}
+          >
             отправить
           </Button>
-        </div>
-      </Paper>
+        </Paper>
+      </Box>
     </>
   );
 }

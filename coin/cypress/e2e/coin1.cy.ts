43879dc5fa829d8ebab72bb9ceb310cef6fb972b
alex8023 <alex8023@yandex.ru>
2023-06-05 12:30:15 +0300

    add test create new account

--- coin/cypress/e2e/coin1.cy.ts
+++ coin/cypress/e2e/coin1.cy.ts
@@ -22,33 +22,30 @@ describe('Проверка авторизации', () => {
     cy.contains('отправить').click();
     cy.get('h1').should('contain.text', 'Ваши счета');
   });
-
-  it('создание счета', () => {
-    // cy.get('#login').type('wrongLogin');
-    // cy.get('#outlined-adornment-password').type('skillbox');
-    cy.contains('создать новый счет').click();
-    // cy.get('div').should('contain.text', 'No such user');
-  });
 });
 
-// describe('Создание нового счета и перевод на него средств', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:4000/login');
-//     cy.get('#login').type('developer');
-//     cy.get('#outlined-adornment-password').type('skillbox');
-//     cy.contains('отправить').click();
-//   });
+describe('Создание нового счета и перевод на него средств', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4000/login');
+    cy.get('#login').type('developer');
+    cy.get('#outlined-adornment-password').type('skillbox');
+    cy.contains('отправить').click();
+  });
 
-//   // it('Неверный пароль', () => {
-//   //   cy.get('#login').type('developer');
-//   //   cy.get('#outlined-adornment-password').type('wrongPassword');
-//   //   cy.contains('отправить').click();
-//   //   cy.get('div').should('contain.text', 'Invalid password');
-//   // });
-//   // it('Успешная авторизация', () => {
-//   //   cy.get('#login').type('developer');
-//   //   cy.get('#outlined-adornment-password').type('skillbox');
-//   //   cy.contains('отправить').click();
-//   //   cy.get('h1').should('contain.text', 'Ваши счета');
-//   // });
-// });
+  it('создание счета', () => {
+    cy.contains('создать новый счет').click();
+  });
+
+  //   // it('Неверный пароль', () => {
+  //   //   cy.get('#login').type('developer');
+  //   //   cy.get('#outlined-adornment-password').type('wrongPassword');
+  //   //   cy.contains('отправить').click();
+  //   //   cy.get('div').should('contain.text', 'Invalid password');
+  //   // });
+  //   // it('Успешная авторизация', () => {
+  //   //   cy.get('#login').type('developer');
+  //   //   cy.get('#outlined-adornment-password').type('skillbox');
+  //   //   cy.contains('отправить').click();
+  //   //   cy.get('h1').should('contain.text', 'Ваши счета');
+  //   // });
+});
